2f0f8b513ab9421fdf46cdc6ad2b3284bdcfbd90
NEngland24 <824864@mystma.org>
2023-03-31 14:50:16 -0500

    3

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 #WRITE YOUR CODE IN THIS FILE
 def countA(word):
-    for i in range(0,len(word)):
-        numA = 0    
+    numA = 0
+    for i in range(0,len(word)):   
         if word[i] == "a":
             numA = numA + 1
-        return numA
-
-
+    return numA
